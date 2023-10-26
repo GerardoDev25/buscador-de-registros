d5b815aa71be633ae0869efd3383dbd1a099e89b
Gerardo Miranda <gerardodev25@gmail.com>
2023-10-25 22:33:22 -0400

    finish update item pending update user

--- js/script.js
+++ js/script.js
@@ -20,18 +20,31 @@ const btnNewItem = document.getElementById('btn-add-item');
 // ** modals
 const modalShowUsers = document.getElementById('modal-show-users');
 const modalAddUser = document.getElementById('modal-add-user');
+const modalEditUser = document.getElementById('modal-edit-user');
 const modalAddItem = document.getElementById('modal-add-item');
+const modalEditItem = document.getElementById('modal-edit-item');
 
-// ** form users
+// ** form users add
 const formAddUser = document.getElementById('form-user');
 const formNameUser = document.getElementById('name-user');
 
-// ** form users
+// ** form users edit
+const formEditUser = document.getElementById('form-user-edit');
+const formEditNameUser = document.getElementById('name-user-edit');
+
+// ** form item
 const formAddItem = document.getElementById('form-item');
 const formprecioItem = document.getElementById('precio-item');
 const formacuentaItem = document.getElementById('acuenta-item');
 const formDetalleItem = document.getElementById('detalle');
 const formSelectUserItem = document.getElementById('select-user');
+
+// ** form item edit
+const formEditItem = document.getElementById('form-item-edit');
+const formEditprecioItem = document.getElementById('precio-item-edit');
+const formEditAcuentaItem = document.getElementById('acuenta-item-edit');
+const formEditDetalleItem = document.getElementById('detalle-edit');
+const formEditSelectUserItem = document.getElementById('select-user-edit');
 
 const loadUserSelected = () => {
   formSelectUserItem.innerHTML = '';
@@ -41,6 +54,17 @@ const loadUserSelected = () => {
     option.textContent = user.nombre;
     formSelectUserItem.appendChild(option);
   }
+};
+const loadUserSelectedEdit = (user) => {
+  formEditSelectUserItem.innerHTML = '';
+  for (const user of users) {
+    const option = document.createElement('option');
+    option.value = user.id;
+    option.textContent = user.nombre;
+    formEditSelectUserItem.appendChild(option);
+  }
+
+  formEditSelectUserItem.value = user.id;
 };
 
 const loadDatatoLocalStorage = () => {
@@ -52,13 +76,27 @@ const loadDatatoLocalStorage = () => {
   }
 };
 
+// todo aqui me quede
+const updateUser = () => {
+  const user = users.find((user) => quitarAcentos(user.nombre) === term);
+  console.log({term});
+  formEditNameUser.innerHTML = user.nombre;
+  modalEditUser.classList.add('modal--active');
+  body.classList.add('overflow--hidden');
+  console.log(formEditNameUser);
+  term = '';
+};
+
 const loadUsers = () => {
   UlUsers.innerHTML = '<div></div>';
   for (const user of users) {
     const li = document.createElement('li');
     li.classList.add('usuario');
     li.dataset.name = user.nombre;
-    li.innerHTML = user.nombre;
+    li.innerHTML = `
+      <p data-search="true">${user.nombre}</p>
+      <button data-edit="true">Editar</button>
+    `;
     UlUsers.firstChild.appendChild(li);
   }
 };
@@ -67,6 +105,7 @@ const printTable = (itemsFiltered) => {
   for (const item of itemsFiltered) {
     const tr = document.createElement('tr');
     tr.classList.add('tabla__tr');
+    tr.dataset.id = item.id;
 
     const user = users.find((user) => user.id === item.idu);
 
@@ -77,6 +116,7 @@ const printTable = (itemsFiltered) => {
         <td class="tabla__td">${item.precio}</td>
         <td class="tabla__td">${item.acuenta}</td>
         <td class="tabla__td">${item.saldo}</td>
+        <td class="tabla__td"><button class="btn-editar">Editar</button></td>
     `;
     tablaBody.appendChild(tr);
   }
@@ -90,11 +130,12 @@ function quitarAcentos(cadena) {
     ó: 'o',
     ú: 'u',
     ü: 'u',
+    ñ: 'n',
   };
 
   return cadena
     .toLowerCase()
-    .replace(/[áéíóúü]/g, (letra) => mapaAcentos[letra] || letra);
+    .replace(/[áéíóúüñ]/g, (letra) => mapaAcentos[letra] || letra);
 }
 
 const searchByTerm = () => {
@@ -151,6 +192,24 @@ btnNewItem.addEventListener('click', () => {
   body.classList.add('overflow--hidden');
 });
 
+tablaBody.addEventListener('click', (e) => {
+  if (!e.target.classList.contains('btn-editar')) return;
+
+  const itemId = Number(e.target.closest('tr').dataset.id);
+  const itemSelected = items.find((item) => item.id === itemId);
+  const userSelected = users.find((user) => user.id === itemSelected.idu);
+
+  loadUserSelectedEdit(userSelected);
+  formEditItem.dataset.itemId = itemSelected.id;
+  formEditItem.dataset.userId = itemSelected.idu;
+  formEditprecioItem.value = itemSelected.precio;
+  formEditAcuentaItem.value = itemSelected.precio - itemSelected.saldo;
+  formEditDetalleItem.textContent = itemSelected.detalle;
+
+  modalEditItem.classList.add('modal--active');
+  body.classList.add('overflow--hidden');
+});
+
 btnShowUsuarios.addEventListener('click', () => {
   modalShowUsers.classList.add('modal--active');
   body.classList.add('overflow--hidden');
@@ -171,13 +230,35 @@ modalAddItem.addEventListener('click', (e) => {
 });
 
 modalShowUsers.addEventListener('click', (e) => {
-  if (e.target.dataset['name']) {
+  if (e.target.classList.contains('usuario')) {
     term = quitarAcentos(e.target.dataset['name']);
     handleClick();
+  }
+  else if (e.target.dataset['search']) {
+    term = quitarAcentos(e.target.parentElement.dataset['name']);
+    handleClick();
+  }
+  else if (e.target.dataset['edit']) {
+    term = quitarAcentos(e.target.parentElement.dataset['name']);
+    updateUser();
   }
 
   modalShowUsers.classList.remove('modal--active');
   body.classList.remove('overflow--hidden');
+});
+
+modalEditItem.addEventListener('click', (e) => {
+  if (
+    e.target.classList.contains('modal') ||
+    e.target.classList.contains('btn-edit-cancel')
+  ) {
+    modalEditItem.classList.remove('modal--active');
+    body.classList.remove('overflow--hidden');
+
+    formEditprecioItem.value = '';
+    formEditAcuentaItem.value = '';
+    formEditDetalleItem.textContent = '';
+  }
 });
 
 formAddItem.addEventListener('submit', (e) => {
@@ -207,6 +288,39 @@ formAddItem.addEventListener('submit', (e) => {
   formacuentaItem.value = '';
   formDetalleItem.value = '';
   modalAddItem.classList.remove('modal--active');
+  body.classList.remove('overflow--hidden');
+});
+
+formEditItem.addEventListener('submit', (e) => {
+  e.preventDefault();
+
+  const fechaRaw = new Date().toLocaleDateString().split('/');
+  const precio = Number(formEditprecioItem.value);
+  const acuenta = Number(formEditAcuentaItem.value);
+  const itemId = Number(formEditItem.dataset.itemId);
+  const idu = Number(formEditSelectUserItem.value);
+  term = quitarAcentos(users.find((user) => user.id === idu).nombre);
+
+  const itemsSelected = items.filter((item) => item.idu === idu);
+
+  const itemUpdated = {
+    idu,
+    fecha: `${fechaRaw[2]}-${fechaRaw[0]}-${fechaRaw[1]}`,
+    detalle: formEditDetalleItem.value,
+    precio,
+    acuenta,
+    saldo: precio - acuenta,
+  };
+
+  items = items.map((item) => {
+    if (item.id === itemId) return { ...item, ...itemUpdated };
+    return item;
+  });
+
+  localStorage.setItem('items', JSON.stringify(items));
+  handleClick();
+
+  modalEditItem.classList.remove('modal--active');
   body.classList.remove('overflow--hidden');
 });
 
